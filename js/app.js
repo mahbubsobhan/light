a0f0edad4fbbc2dcadfb7aad37b596641a92a642
mahbubsobhan <146010090+mahbubsobhan@users.noreply.github.com>
2024-05-08 20:06:20 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let on = document.querySelector(`.on`)
+let img = document.querySelector(`.img`)
+let off = document.querySelector(`.off`)
+
+on.addEventListener(`click`,function(){
+  img.src = `https://www.w3schools.com/js/pic_bulbon.gif`
+})
+off.addEventListener(`click`,function(){
+  img.src = `https://www.w3schools.com/js/pic_bulboff.gif`
+})
+
